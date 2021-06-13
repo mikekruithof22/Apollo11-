4abec2e3e9566f059797723286a1a6e362c31f26
LAPTOP-GDRVJF78\mikek <mikekruithof22@hotmail.com>
2021-06-13 15:53:04 +0200

    We have lift off

---
+++ Apollo11/Apollo11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Apollo11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ExchangeService - Get Candles
+            // FileService - Save Canldes
+            // File Fesvice - Get All Relevant canldes
+            // DataProcessor - Calculate RSI
+            // DataProcessor - Calculate RSI Delta
+            // DataProcessor - CalculatePrice Delta
+            // BUY?
+            // ExchangeService - Buy Bitcoin
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Apollo11/Apollo11/Services/ExchangeService.cs
@@ -0,0 +1,30 @@
+﻿using Apollo11.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apollo11.Services
+{
+    public class ExchangeService
+    {
+        private readonly string endpoint = "GetCandles";
+        public async Task<List<Candle>> GetCandlesAsync()
+        {
+            var client = GetClient();
+            var response = await client.GetAsync(endpoint);
+            var candles = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Candle>>(candles);
+        }
+
+        private HttpClient GetClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(ConfigurationManager.AppSettings.Get("BaseAddress"));
+            return client;
+        }
+    }
+}
