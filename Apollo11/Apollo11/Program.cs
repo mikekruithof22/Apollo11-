ecbb998ee518145414727a24e655e18e917e1187
a.gulzadian@gmail.com <a.gulzadian@gmail.com>
2021-06-23 20:34:47 +0200

    first draft calculate deltas

--- Apollo11/Apollo11/Program.cs
+++ Apollo11/Apollo11/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Apollo11.Helpers;
+using Apollo11.Models;
 using Apollo11.Services;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Apollo11
 {
@@ -10,7 +13,7 @@ namespace Apollo11
         {
             var exchangeService = new ExchangeService();
             var candles = exchangeService.GetCandlesAsync().Result;
-            var rsi = PriceEngine.Launch(candles);
+            var rsis = PriceEngine.Launch(candles);
             // ExchangeService - Get Candles
             // FileService - Save Canldes, Save calculated RSI
             // File Fesvice - Get All Relevant canldes
@@ -20,10 +23,44 @@ namespace Apollo11
             // BUY?
             // ExchangeService - Buy Bitcoin
             Console.WriteLine("Hello World!");
-            var fileService = new FileService();
-            fileService.EnsureTables();
+            // var fileService = new FileService();
+            // fileService.EnsureTables();
             // fileService.WriteCandleToTable();
-            fileService.ReadCellFromTable("");
+            // fileService.ReadCellFromTable("");
+
+            GetDivergences(candles, rsis);
         }
+
+        private static void GetDivergences(List<Candlestick> candles, double[] rsis)
+        {
+//            Console.WriteLine($"lengths: candles={candles.Count} rsis={rsis.Length}");
+
+            var backTrackCount = 25;
+
+            for (int i = 0; i < backTrackCount; i++)
+            {
+                var matchup = new Matchup();
+
+                var currentRsi = rsis[rsis.Length - 1];
+                var comparisonRsi = rsis[rsis.Length - 1];
+                var rsiDelta = currentRsi - comparisonRsi;
+
+                var currentPrice = candles[candles.Count - 1].Close;
+                var comparisonPrice = candles[candles.Count - 1].Close;
+                var PriceDelta = currentPrice - comparisonPrice;
+
+                if (rsiDelta > 0 && PriceDelta < 0)
+                {
+                    Console.WriteLine($"DIVERGENCE: rsiDelta={rsiDelta} PriceDelta={PriceDelta}");
+
+                }
+            }
+        }
+    }
+
+    public class Matchup
+    {
+        public double rsiDelta { get; set; }
+        public double priceDelta { get; set; }
     }
 }
